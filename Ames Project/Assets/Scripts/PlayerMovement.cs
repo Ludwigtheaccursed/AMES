2a2a077b8214f0682d450d4d3f0a53a1dfa8e010
Chaela <chaelabird@gmail.com>
2025-03-20 14:00:48 -0700

    Started adding basic player movement

--- Ames Project/Assets/Scripts/PlayerMovement.cs
+++ Ames Project/Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float moveSpeed;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+    Rigidbody rb;
+
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void MyInput()
+    {
+
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
     }
 }
