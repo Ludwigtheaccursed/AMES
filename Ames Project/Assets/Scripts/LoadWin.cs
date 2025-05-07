21ae90d26c6296950fa91c67ccb163d2c196e2fe
Chaela <chaelabird@gmail.com>
2025-05-07 12:29:50 -0700

    eggmuffins

--- Ames Project/Assets/Scripts/LoadWin.cs
+++ Ames Project/Assets/Scripts/LoadWin.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadWin : MonoBehaviour
 {
+    [SerializeField]
+    string WinScene = "";
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+  
+    private void OnCollisionEnter(Collision collision)
     {
-        
-    }
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(WinScene);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+        }
     }
 }
